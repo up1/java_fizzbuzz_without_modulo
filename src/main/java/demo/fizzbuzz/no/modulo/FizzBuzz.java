fdeacf50fe1db3eba837d3db9a8225265ee16db5
somkiat <somkiat.p@gmail.com>
2014-08-31 23:05:49 +0700

    Resolve modulo by five with stupid solution

--- src/main/java/demo/fizzbuzz/no/modulo/FizzBuzz.java
+++ src/main/java/demo/fizzbuzz/no/modulo/FizzBuzz.java
@@ -3,12 +3,31 @@ package demo.fizzbuzz.no.modulo;
 public class FizzBuzz {
 
 	public String say(int input) {
-		for(int number = 0; number<= input; number+=3) {
-			if( number == input ) {
-				return "Fizz";
-			}
+		if(isFizz(input)) {
+			return "Fizz";
+		}
+		if(isBuzz(input)) {
+			return "Buzz";
 		}
 		return String.valueOf(input);
+	}
+	
+	private boolean isBuzz(int input) {
+		for(int number = 0; number<= input; number+=5) {
+			if( number == input ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isFizz(int input) {
+		for(int number = 0; number<= input; number+=3) {
+			if( number == input ) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
