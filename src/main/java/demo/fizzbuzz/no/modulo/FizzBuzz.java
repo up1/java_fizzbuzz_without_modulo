b6b7aaf30c17a791d3aaba9972f85ca6d0651720
somkiat <somkiat.p@gmail.com>
2014-08-31 23:02:34 +0700

    Resolve modulo by three with stupid solution

--- src/main/java/demo/fizzbuzz/no/modulo/FizzBuzz.java
+++ src/main/java/demo/fizzbuzz/no/modulo/FizzBuzz.java
@@ -3,7 +3,12 @@ package demo.fizzbuzz.no.modulo;
 public class FizzBuzz {
 
 	public String say(int input) {
-		return null;
+		for(int number = 0; number<= input; number+=3) {
+			if( number == input ) {
+				return "Fizz";
+			}
+		}
+		return String.valueOf(input);
 	}
 
 }
